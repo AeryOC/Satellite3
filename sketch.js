fd88ad3a4453808d72953037ec9c4d3467a01439
Aery <167365202+AeryOC@users.noreply.github.com>
2024-04-24 15:57:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,12 +7,12 @@ function preload(){
 function setup(){
     createCanvas (1920,1080);
     colorMode(HSB);
-    imageMode(CENTER);
+    //imageMode(CENTER);
 
     s = "bla bla bla"
 }
 
 function draw(){
-    image(img1,100,100);
+    image(img1,0,0,1920,1080);
     text(s,0,40);
 }
