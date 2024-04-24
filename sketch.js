eaaad3d9746b41d241165f08f664ef790e55d681
samss716 <167812703+samss716@users.noreply.github.com>
2024-04-24 16:03:08 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 let img1;
+let img2;
 
 function preload(){
     img1 = loadImage('resources/media/testimage.png')
+    img2 = loadImage('resources/media/
 }
+
 
 function setup(){
     createCanvas (1920,1080);
@@ -15,4 +18,6 @@ function setup(){
 function draw(){
     image(img1,0,0,1920,1080);
     text(s,0,40);
+
+    
 }
