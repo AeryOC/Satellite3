ee4ec89b35dee503c755761287ad840982193c45
Aery <167365202+AeryOC@users.noreply.github.com>
2024-04-24 16:15:12 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 let img1;
 let img2;
 let img3; 
+let img4;
 
 function preload(){
     img1 = loadImage('resources/media/testimage.png');
     img2 = loadImage('resources/media/planet-unscreen.gif');
     img3 = loadImage('resources/media/portal-unscreen.gif');
+    img4 = loadImage('resources/media/sat1.gif');
 }
 
 
@@ -19,6 +21,7 @@ function setup(){
 
 function draw(){
     image(img1,width,height);
+    image(img4,0,0);
     text(s,0,40);
 
 }
