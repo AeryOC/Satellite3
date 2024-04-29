9e52d0eb60963fd799a57aed4631a8b9ff0ff7d9
Aery <167365202+AeryOC@users.noreply.github.com>
2024-04-29 17:01:57 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,11 +5,11 @@ let img4;
 let img5;
 
 function preload(){
-    img1 = loadImage('resources/media/testimage.png');
-    img2 = loadImage('resources/media/planet-unscreen.gif');
-    img3 = loadImage('resources/media/portal-unscreen.gif');
-    img4 = loadImage('resources/media/sattransparent.gif');
-    img5 = loadImage('resources/media/planet1.gif');
+    img1 = loadImage('resources/media/testimage.png'); //placeholder
+    //img2 = loadImage('resources/media/planet-unscreen.gif'); //Sam's planet
+    img3 = loadImage('resources/media/portal-unscreen.gif'); //Sam's portal
+    img4 = loadImage('resources/media/sattransparent.gif'); //Aery's satellite
+    img5 = loadImage('resources/media/planet1.gif'); // Aiden's planet
 }
 
 
